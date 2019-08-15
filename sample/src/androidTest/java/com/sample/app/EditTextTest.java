007c5eedc04c87d48b9e91b5e89a743d4a15b72e
Hannes Achleitner <hannes.software@gmx.at>
2019-08-15 18:44:23 +0200

    make use of Moka

--- sample/src/androidTest/java/com/sample/app/EditTextTest.java
+++ sample/src/androidTest/java/com/sample/app/EditTextTest.java
@@ -7,13 +7,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
+//import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.moka.EspressoMoka.onView;
 
 @RunWith(AndroidJUnit4.class)
 public class EditTextTest {
@@ -31,7 +32,7 @@ public class EditTextTest {
         onView(withId(R.id.changeTextBt)).perform(click());
 
         // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
+        onView(withId(R.id.textToBeChanged)).checkMatches(withText(STRING_TO_BE_TYPED));
     }
 
 }
